382edd4ee2f2bc043cc2d287572ba156853d6375
Yunkeun <yunx2@naver.com>
2022-01-11 18:04:44 +0900

    fix: 클래스 변수 정의

--- src/main/java/com/yoonveloping/springordersystem/order/OrderServiceImpl.java
+++ src/main/java/com/yoonveloping/springordersystem/order/OrderServiceImpl.java
@@ -1,13 +1,15 @@
 package com.yoonveloping.springordersystem.order;
 
 import com.yoonveloping.springordersystem.discount.DiscountPolicy;
+import com.yoonveloping.springordersystem.discount.FixDiscountPolicy;
 import com.yoonveloping.springordersystem.member.Member;
 import com.yoonveloping.springordersystem.member.MemberRepository;
+import com.yoonveloping.springordersystem.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-	private MemberRepository memberRepository;
-	private DiscountPolicy discountPolicy;
+	private MemberRepository memberRepository = new MemoryMemberRepository();
+	private DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
