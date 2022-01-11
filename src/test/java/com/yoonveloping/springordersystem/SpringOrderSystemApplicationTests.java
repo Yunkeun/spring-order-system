ad972abd27283a1551af20d0189384304d779e2a
Yunkeun <yunx2@naver.com>
2022-01-11 16:10:12 +0900

    Initial commit

---
+++ src/test/java/com/yoonveloping/springordersystem/SpringOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.springordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
