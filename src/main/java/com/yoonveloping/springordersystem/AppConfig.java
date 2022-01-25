d80d94783fad08e96f9e6ff4d8de497743b2cfc1
Yunkeun <yunx2@naver.com>
2022-01-25 18:57:38 +0900

    refactor: AppConfig 파일 내 역할과 구현이 드러나게 리팩토링

--- src/main/java/com/yoonveloping/springordersystem/AppConfig.java
+++ src/main/java/com/yoonveloping/springordersystem/AppConfig.java
@@ -1,6 +1,8 @@
 package com.yoonveloping.springordersystem;
 
+import com.yoonveloping.springordersystem.discount.DiscountPolicy;
 import com.yoonveloping.springordersystem.discount.RateDiscountPolicy;
+import com.yoonveloping.springordersystem.member.MemberRepository;
 import com.yoonveloping.springordersystem.member.MemberService;
 import com.yoonveloping.springordersystem.member.MemberServiceImpl;
 import com.yoonveloping.springordersystem.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.yoonveloping.springordersystem.order.OrderServiceImpl;
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
 	}
 }
