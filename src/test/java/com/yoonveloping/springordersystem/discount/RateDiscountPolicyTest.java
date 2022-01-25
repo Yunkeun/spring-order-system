905d697ba46517409b008e9fefbf5e1d85629883
Yunkeun <yunx2@naver.com>
2022-01-25 18:01:29 +0900

    test: 정률할인정책 테스트

---
+++ src/test/java/com/yoonveloping/springordersystem/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package com.yoonveloping.springordersystem.discount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.yoonveloping.springordersystem.member.Grade;
+import com.yoonveloping.springordersystem.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RateDiscountPolicyTest {
+
+	RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+	// 성공 Test
+	@Test
+	@DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+	void vip_o() {
+		// given
+		Member member = new Member(1L, "memberVIP", Grade.VIP);
+		// when
+		int discount = discountPolicy.discount(member, 10000);
+		// then
+		Assertions.assertThat(discount).isEqualTo(1000);
+	}
+
+	// 실패 Test
+	@Test
+	@DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+	void vip_x() {
+		// given
+		Member member = new Member(1L, "memberBasic", Grade.BASIC);
+		// when
+		int discount = discountPolicy.discount(member, 10000);
+		// then
+		Assertions.assertThat(discount).isEqualTo(0);
+	}
+}
