81785a21f0a0a895456c3a0eafda4becbdbc5832
Yunkeun <yunx2@naver.com>
2022-01-26 16:57:04 +0900

    test: 빈 조회 테스트

---
+++ src/test/java/com/yoonveloping/springordersystem/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package com.yoonveloping.springordersystem.beanfind;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.yoonveloping.springordersystem.AppConfig;
+import com.yoonveloping.springordersystem.member.MemberService;
+import com.yoonveloping.springordersystem.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+	@Test
+	@DisplayName("빈 이름으로 조회")
+	void findBeanByName() {
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("이름 없이 타입으로만 조회")
+	void findBeanByType() {
+		MemberService memberService = ac.getBean(MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("구체 타입으로 조회")
+	void findBeanByName2() {
+		// 구체 타입으로 조회 시 유연성이 떨어진다.
+		MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("빈 이름으로 조회X")
+	void findBeanByName_X() {
+		assertThrows(NoSuchBeanDefinitionException.class,
+			() -> ac.getBean("x", MemberService.class));
+	}
+}
