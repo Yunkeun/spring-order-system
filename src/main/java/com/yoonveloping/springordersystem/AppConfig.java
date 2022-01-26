97ebd7ac4ef4f9ce6815d9ea4332fa70cb15c12c
Yunkeun <yunx2@naver.com>
2022-01-26 16:19:40 +0900

    refactor: 스프링을 이용하여 설정 정보 메소드를 스프링 컨테이너에 등록

--- src/main/java/com/yoonveloping/springordersystem/AppConfig.java
+++ src/main/java/com/yoonveloping/springordersystem/AppConfig.java
@@ -8,21 +8,28 @@ import com.yoonveloping.springordersystem.member.MemberServiceImpl;
 import com.yoonveloping.springordersystem.member.MemoryMemberRepository;
 import com.yoonveloping.springordersystem.order.OrderService;
 import com.yoonveloping.springordersystem.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new RateDiscountPolicy();
 	}
